5195da1247b134e82ba054c1cd117a03fd6cd46c
Mikle95 <1mikle1@mail.ru>
2022-04-16 16:51:32 +0300

    taskmanager

--- app/src/main/java/com/MonitoringApp/ui/taskmanager/TaskmanagerFragment.java
+++ app/src/main/java/com/MonitoringApp/ui/taskmanager/TaskmanagerFragment.java
@@ -1,41 +1,26 @@
 package com.MonitoringApp.ui.taskmanager;
 
-import android.app.AlertDialog;
+
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.MonitoringApp.API.LoginController;
-import com.MonitoringApp.API.TasksApiController;
 import com.MonitoringApp.API.data.Project;
 import com.MonitoringApp.R;
-import com.MonitoringApp.databinding.FragmentDashboardBinding;
 import com.MonitoringApp.databinding.TaskManagerBinding;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 public class TaskmanagerFragment extends Fragment {
 
@@ -49,30 +34,35 @@ public class TaskmanagerFragment extends Fragment {
         View root = binding.getRoot();
 
         adapter = new ProjectAdapter(getContext(), android.R.layout.simple_list_item_1, R.id.textView2, new ArrayList<Project>());
-        Project a = new Project();
-        a.project_creator_login = "Mikle";
-        a.project_name = "Some project";
-        adapter.add(a);
-        Project b = new Project();
-        b.project_creator_login = "Mikle";
-        b.project_name = "Some project";
-        adapter.add(b);
 
-
+        // TODO: -- REMOVE
+        test(adapter, 10);
         binding.listView.setAdapter(adapter);
 
         return root;
+    }
+
+    // TODO: -- REMOVE
+    public static void test(ProjectAdapter adapter, int count){
+        for (int i = 0; i < count; ++i){
+            Project a = new Project();
+            a.project_creator_login = "Mikle";
+            a.project_name = "Some project";
+            adapter.add(a);
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        adapter = null;
     }
 
 
     private static class ProjectAdapter extends ArrayAdapter<Project> {
         ArrayList<Project> items;
+        View activeView;
         public ProjectAdapter(Context context, int resource, int textViewResourceId, ArrayList items){
             super(context, resource, textViewResourceId, items);
             this.items = items;
@@ -91,16 +81,37 @@ public class TaskmanagerFragment extends Fragment {
             text.setText(item.project_name + "\n" +
                     item.project_creator_login);
 
+            ListView listView = row.findViewById(R.id.taskList);
+            Button btn1 = row.findViewById(R.id.button2);
+            Button btn2 = row.findViewById(R.id.button3);
+
             row.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    view.
-//                    row.findViewById(R.id.taskList);
+                    if (listView.getAdapter() != null && ((ProjectAdapter)listView.getAdapter()).getCount() > 0) {
+                        btn1.setVisibility(View.GONE);
+                        btn2.setVisibility(View.GONE);
+                        closeRow(row);
+                        return;
+                    }
+                    btn1.setVisibility(View.VISIBLE);
+                    btn2.setVisibility(View.VISIBLE);
+
+                    ProjectAdapter adapter = new ProjectAdapter(getContext(), android.R.layout.simple_list_item_1, R.id.textView2, new ArrayList<Project>());
+                    test(adapter, 20);
+
+                    listView.setAdapter(adapter);
                 }
             });
 
-
+            if (items.stream().count() == 1)
+                row.callOnClick();
             return row;
+        }
+
+        public void closeRow(View row){
+            ListView listView = row.findViewById(R.id.taskList);
+            ((ProjectAdapter)listView.getAdapter()).clear();
         }
     }
 }
